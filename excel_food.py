7c59b6f70896812ebb997b1c9af1de1a51599fc1
Jake Attard <jakeattard18@gmail.com>
2019-09-21 22:57:54 +1000

    Task 3 complete with the excel file.

---
+++ excel_food.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 22:38:21 2019
+
+@author: JakeC
+"""
+import openpyxl as xl
+from openpyxl import Workbook
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+wb = Workbook()
+wbs1 = wb.active
+
+wbs1.title = "Violation Types"
+wbs1["A1"] = "Code"
+wbs1["B1"] = "Description"
+wbs1["C1"] = "Count"
+
+print(wb.sheetnames)
+
+sqlSelectQuery = """
+SELECT violation_code,
+    violation_description,
+    COUNT(violation_code) FROM violations GROUP BY violation_code ORDER BY violation_code"""
+
+cursor.execute(sqlSelectQuery)
+
+violationTypes = cursor.fetchall()
+
+for a, b in zip(wbs1.iter_rows(min_row = 2, min_col = 1, max_col = 3, max_row = 50), violationTypes):
+    for i in range(3):
+        a[i].value = b[i]
+
+wb.save("ViolationTypes.xlsx")
