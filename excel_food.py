c195d2ab2d636c0d2ca9b84aebc88994278ffcb3
Jake Attard <jakeattard18@gmail.com>
2019-10-01 12:33:12 +1000

    Task 3 added with total sum of the count.

--- excel_food.py
+++ excel_food.py
@@ -30,8 +30,21 @@ cursor.execute(sqlSelectQuery)
 
 violationTypes = cursor.fetchall()
 
-for a, b in zip(wbs1.iter_rows(min_row = 2, min_col = 1, max_col = 3, max_row = 200), violationTypes):
-    for i in range(3):
-        a[i].value = b[i]
+for i in violationTypes:
+    wbs1.append(i)
 
+#for a, b in zip(wbs1.iter_rows(min_row = 2, min_col = 1, max_col = 3), violationTypes):
+#    for i in range(3):
+#        a[i].value = b[i]
+        
+counter = 0
+for row in wbs1.iter_rows(min_row = 2):
+    count = row[2].value
+    counter += count
+
+lastRow = wbs1.max_row
+
+wbs1["B118"] = "Total Sum"
+
+wbs1['C' + str(lastRow + 1) ] = int(counter)
 wb.save("ViolationTypes.xlsx")
