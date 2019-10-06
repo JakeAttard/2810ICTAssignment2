edd391f437fc4283ba4067719baaade749eba8bb
Jake Attard <jakeattard18@gmail.com>
2019-10-06 16:48:59 +1000

    Changes to task 4 part 1, 2, 3. Added comments.

--- numpy_food.py
+++ numpy_food.py
@@ -2,55 +2,59 @@ import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
 
-# Check connection
-con = sqlite3.connect("database.db")
-print('Connection: OK')
+# Connecting to the database
+connection = sqlite3.connect("database.db")
 
-#''' Question 1 '''
+# Testing to see if the connection is successful with the database
+print('Connection successful')
 
-# Fetch data.
+#Task 4 Part 1
+#The number of violations per month for the postcode with the highest total violations
+
+# Fetching data from the database
 query1 = "select facility_zip,strftime('%m',activity_date) as month, count(violations.serial_number) as num_of_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number and facility_zip=(select facility_zip from inspections, violations where violations.serial_number=inspections.serial_number group by facility_zip order by count(violations.serial_number) desc limit 1) group by month"
-df1 = pd.read_sql_query(query1, con)
+figure1 = pd.read_sql_query(query1, connection)
 
 # Plot the graph.
 plt.figure(figsize=(8, 8))
-plt.bar(df1.month, df1.num_of_viol_per_month)
+plt.bar(figure1.month, figure1.num_of_viol_per_month)
 plt.title(
-    f"Violations per month for zip code {df1.facility_zip[0]}\n which has the highest total violations")
+    f"Violations per month for zip code {figure1.facility_zip[0]}\n which has the highest total violations")
 plt.ylabel("Number of violations")
-plt.xlabel("Month")
+plt.xlabel("Months")
 plt.show()
-###############################################################
 
-#''' Question 2 '''
+# Task 4 Part 2
+# The number of violations per month for the postcode with the lowest total violations
 
-# Fetch data.
+# Fetching data from the database
 query2 = "select facility_zip,strftime('%m',activity_date) as month, count(violations.serial_number) as num_of_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number and facility_zip=(select facility_zip from inspections, violations where violations.serial_number=inspections.serial_number group by facility_zip order by count(violations.serial_number) asc limit 1) group by month"
-df2 = pd.read_sql_query(query2, con)
+figure2 = pd.read_sql_query(query2, connection)
 
 # Plot the graph.
 plt.figure(figsize=(8, 8))
-plt.bar(df2.month, df2.num_of_viol_per_month)
+plt.bar(figure2.month, figure2.num_of_viol_per_month)
 plt.title(
-    f"Violations per month for zip code {df2.facility_zip[0]}\n which has the lowest total violations")
+    f"Violations per month for zip code {figure2.facility_zip[0]}\n which has the lowest total violations")
 plt.ylabel("Number of violations")
-plt.xlabel("Month")
+plt.xlabel("Months")
 plt.show()
 
-###############################################################
-#''' Question 3 '''
+# Task 4 Part 3
+# The average number of violations per month for all McDonalds compared with the average number of violations for all burger kings.
 
-# Fetch data.
+# Fetching data from the database
 query3 = "select strftime('%m',activity_date) as month, count(violations.serial_number)/count(distinct(facility_zip)) as avg_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number group by month"
-df3 = pd.read_sql_query(query3, con)
+figure3 = pd.read_sql_query(query3, connection)
 
 # Plot the graph.
 plt.figure(figsize=(8, 8))
-plt.bar(df3.month, df3.avg_viol_per_month)
+plt.bar(figure3.month, figure3.avg_viol_per_month)
 plt.title("Average number of violations per month\n for all of California")
 plt.ylabel("Average")
 plt.xlabel("Month")
 plt.show()
+
 ###############################################################
 
 #''' Plot the three first graphs together '''
@@ -90,8 +94,8 @@ query41 = "select strftime('%m',activity_date) as month, count(violations.serial
 query42 = "select strftime('%m',activity_date) as month, count(violations.serial_number)/(select count(distinct(facility_name)) from inspections where facility_name LIKE '%MCDONALDS%') as avg_num_viol_pm_MC from violations, inspections where violations.serial_number=inspections.serial_number and facility_name LIKE '%MCDONALDS%'group by month"
 
 # Fetch data.
-df41 = pd.read_sql_query(query41, con)
-df42 = pd.read_sql_query(query42, con)
+df41 = pd.read_sql_query(query41, connection)
+df42 = pd.read_sql_query(query42, connection)
 
 # Plot the graph.
 plt.figure(figsize=(15, 15))
@@ -107,5 +111,5 @@ plt.ylabel('Average for\n McDonalds')
 
 plt.show()
 ###############################################################
-con.close()
+connection.close()
 print('Connection: Closed')
