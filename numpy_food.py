4c2f96be99c73839e11120affa4be6bd7e44864d
Jake Attard <jakeattard18@gmail.com>
2019-10-06 15:18:26 +1000

    Changes / fixes to task 4.

--- numpy_food.py
+++ numpy_food.py
@@ -1,119 +1,111 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 22 15:34:03 2019
-
-@author: JakeC
-"""
-
-import numpy
+import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
-import datetime
-from operator import itemgetter
 
-connection = sqlite3.connect("database.db")
-cursor = connection.cursor()
+# Check connection
+con = sqlite3.connect("database.db")
+print('Connection: OK')
 
-violationsTotalPostcode = dict()
-violationsTotalMonth = dict()
-violationsMonthAverage = list()
-violationsPostcode = list()
-violationsDifferentPostcodes = list()
-lastPostcode = 0
-minimumValue = 0
-difference = 0
+#''' Question 1 '''
 
-sqlSelectQuery = """
-SELECT COUNT(violations.serial_number),
-    facility_zip,
-    strftime('%m-%Y', activity_date) FROM violations,
-    inspections WHERE violations.serial_number=inspections.serial_number GROUP BY facility_zip,
-    strftime('%m', activity_date) ORDER BY facility_zip,
-    strftime('%Y-%m', activity_date) DESC"""
-    
-cursor.execute(sqlSelectQuery)
-data = cursor.fetchall()
+# Fetch data.
+query1 = "select facility_zip,strftime('%m',activity_date) as month, count(violations.serial_number) as num_of_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number and facility_zip=(select facility_zip from inspections, violations where violations.serial_number=inspections.serial_number group by facility_zip order by count(violations.serial_number) desc limit 1) group by month"
+df1 = pd.read_sql_query(query1, con)
 
-for i in data:
-    violationsTotalPostcode[i[1]] = violationsTotalPostcode.get(i[1], 0) + i[0]
-    violationsTotalMonth[i[2]] = violationsTotalMonth.get(i[2], [])
-    violationsTotalMonth[i[2]].append(i[0])
-    if i[1] == lastPostcode:
-        if difference < (abs(i[0] - minimumValue)):
-            difference = (abs(i[0] - minimumValue))
-            postcodeDifference = i[1]
-        elif minimumValue > i[0]:
-            minimumValue = i[0]
-    lastPostcode = i[1]
-    
-for key, value in violationsTotalMonth.items():
-    violationsMonthAverage.append([key, sum(value) / len(value)])
+# Plot the graph.
+plt.figure(figsize=(8, 8))
+plt.bar(df1.month, df1.num_of_viol_per_month)
+plt.title(
+    f"Violations per month for zip code {df1.facility_zip[0]}\n which has the highest total violations")
+plt.ylabel("Number of violations")
+plt.xlabel("Month")
+plt.show()
+###############################################################
 
-largestAmountViolations = max(violationsTotalPostcode, key = violationsTotalPostcode.get)
+#''' Question 2 '''
 
-for j in data:
-    if j[1] == largestAmountViolations:
-        violationsPostcode.append((j[2], j[0]))
-    if postcodeDifference == j[1]:
-        violationsDifferentPostcodes.append((j[2], j[0]))
-        
-for date in violationsMonthAverage:
-    date[0] = date[0][3:] + '-' + date[0][:2]
-violationsMonthAverage = sorted(violationsMonthAverage)
+# Fetch data.
+query2 = "select facility_zip,strftime('%m',activity_date) as month, count(violations.serial_number) as num_of_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number and facility_zip=(select facility_zip from inspections, violations where violations.serial_number=inspections.serial_number group by facility_zip order by count(violations.serial_number) asc limit 1) group by month"
+df2 = pd.read_sql_query(query2, con)
 
-violationsPostcode.reverse()
-violationsDifferentPostcodes.reverse()
-
-# ViolationsPostcode
-valueX = [x[0] for x in violationsPostcode]
-valueY = [y[1] for y in violationsPostcode]
-plt.xticks(range(len(valueY)), valueX)
-plt.plot(valueY)
-plt.title(('Violations per month for zip code {} which has the highest total violations').format(largestAmountViolations))
-plt.xlabel('Date')
-plt.ylabel('Number of violations')
+# Plot the graph.
+plt.figure(figsize=(8, 8))
+plt.bar(df2.month, df2.num_of_viol_per_month)
+plt.title(
+    f"Violations per month for zip code {df2.facility_zip[0]}\n which has the lowest total violations")
+plt.ylabel("Number of violations")
+plt.xlabel("Month")
 plt.show()
 
-valueX = [x[0] for x in violationsDifferentPostcodes]
-valueY = [y[1] for y in violationsDifferentPostcodes]
-plt.xticks(range(len(valueY)), valueX)
-plt.plot(valueY)
-plt.title(('Violations per month for zip code {} which has the greatest variance').format(postcodeDifference))
-plt.xlabel('Date')
-plt.ylabel('Number of violations')
+###############################################################
+#''' Question 3 '''
+
+# Fetch data.
+query3 = "select strftime('%m',activity_date) as month, count(violations.serial_number)/count(distinct(facility_zip)) as avg_viol_per_month from inspections, violations where violations.serial_number=inspections.serial_number group by month"
+df3 = pd.read_sql_query(query3, con)
+
+# Plot the graph.
+plt.figure(figsize=(8, 8))
+plt.bar(df3.month, df3.avg_viol_per_month)
+plt.title("Average number of violations per month\n for all of California")
+plt.ylabel("Average")
+plt.xlabel("Month")
+plt.show()
+###############################################################
+
+#''' Plot the three first graphs together '''
+
+# Plot the graph.
+plt.figure(figsize=(13, 13))
+plt.subplot(2, 2, 1)
+plt.bar(df1.month, df1.num_of_viol_per_month)
+plt.title(
+    f"Violations per month for zip code {df1.facility_zip[0]}\n which has the highest total violations")
+plt.ylabel("Number of violations")
+plt.xlabel("Month")
+
+plt.subplot(2, 2, 2)
+
+plt.bar(df2.month, df2.num_of_viol_per_month)
+plt.title(
+    f"Violations per month for zip code {df2.facility_zip[0]}\n which has the lowest total violations")
+plt.ylabel("Number of violations")
+plt.xlabel("Month")
+
+plt.subplot(2, 2, 3)
+plt.bar(df3.month, df3.avg_viol_per_month)
+plt.title("Average number of violations per month\n for all of California")
+plt.ylabel("Average")
+plt.xlabel("Month")
+
 plt.show()
 
-valueX =[x[0].replace('20','') for x in violationsMonthAverage]
-valueY = [y[1] for y in violationsMonthAverage]
-plt.xticks(range(len(valueY)), valueX)
-plt.plot(valueY)
-plt.title('Total violations per month for all zip codes')
-plt.xlabel('Date')
-plt.ylabel('Number of violations')
+
+#''' Question 4 '''
+
+# Average number of violations per month for all Burger Kings
+query41 = "select strftime('%m',activity_date) as month, count(violations.serial_number)/(select count(distinct(facility_name)) from inspections where facility_name LIKE '%BURGER KING%') as avg_num_viol_pm_BK from violations, inspections where violations.serial_number=inspections.serial_number and facility_name LIKE '%BURGER KING%' group by month"
+
+# Average number of violations per month for all Macdonalds
+query42 = "select strftime('%m',activity_date) as month, count(violations.serial_number)/(select count(distinct(facility_name)) from inspections where facility_name LIKE '%MCDONALDS%') as avg_num_viol_pm_MC from violations, inspections where violations.serial_number=inspections.serial_number and facility_name LIKE '%MCDONALDS%'group by month"
+
+# Fetch data.
+df41 = pd.read_sql_query(query41, con)
+df42 = pd.read_sql_query(query42, con)
+
+# Plot the graph.
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 1, 1)
+plt.bar(df41.month, df41.avg_num_viol_pm_BK)
+plt.title('Average number of violations per month\n for Burger Kings and MCDonalds')
+plt.ylabel('Average for\n Burger King')
+
+plt.subplot(2, 1, 2)
+plt.bar(df42.month, df42.avg_num_viol_pm_MC, color='green')
+plt.xlabel('Month')
+plt.ylabel('Average for\n McDonalds')
+
 plt.show()
-
-#### Average number of violations per month for all McDonalds compared with average for all Burger Kings.
-
-query = """SELECT COUNT(violations.serial_number), strftime('%Y-%m', activity_date), facility_name
-FROM violations, inspections
-WHERE violations.serial_number=inspections.serial_number AND (facility_name LIKE '%BURGER KING%' OR facility_name LIKE '%MCDONALDS%')
-GROUP BY facility_name, strftime('%m',activity_date)
-ORDER BY  facility_name , strftime('%Y-%m', activity_date) DESC
-"""
-cursor.execute(query)
-result = cursor.fetchall()
-
-restraunt = dict()
-x = list()
-y = list()
-for item in result:
-    restraunt[item[1]] = restraunt.get(item[1], 0) + item[0]
-for key, value in restraunt.items():
-    x.append(key)
-    y.append(value)
-plt.xticks(range(len(valueY)), valueX)
-plt.plot(valueX)
-plt.title('Total violations per month for all burger kings and mcdonalds')
-plt.xlabel('Date')
-plt.ylabel('Number of violations')
-plt.show()
+###############################################################
+con.close()
+print('Connection: Closed')
